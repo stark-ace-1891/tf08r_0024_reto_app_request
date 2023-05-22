379dee2c94fd0ff9c48d038dd280e1d69ed52f7b
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-05-22 12:20:28 -0500

    Acabando Container y cambiando color de container de fondo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,19 +3,21 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
         width: double.infinity,
-        color: const Color(0xffD5F3F4),
+        color: const Color(0xffD8DFF6),
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(
-              horizontal: 30,
+              horizontal: 15,
               vertical: 22,
             ),
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.5),
+                color: Color(0xffF3F6FF),
                 borderRadius: BorderRadius.circular(30),
               ),
               child: Padding(
@@ -65,7 +67,7 @@ class HomePage extends StatelessWidget {
                     ),
                     Container(
                       width: double.infinity,
-                      height: 250,
+                      height: height * 0.29,
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(32),
@@ -78,7 +80,7 @@ class HomePage extends StatelessWidget {
                               horizontal: 23,
                             ),
                             decoration: BoxDecoration(
-                              color: Colors.blue,
+                              color: Color(0xff256FFF),
                               borderRadius: BorderRadius.only(
                                 topLeft: Radius.circular(32),
                                 topRight: Radius.circular(32),
@@ -120,6 +122,108 @@ class HomePage extends StatelessWidget {
                                   ],
                                 ),
                               ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            child: Expanded(
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: ListTile(
+                                          leading: ClipRRect(
+                                            borderRadius: BorderRadius.circular(
+                                                20), // Image border
+                                            child: SizedBox.fromSize(
+                                              size: Size.fromRadius(
+                                                  30), // Image radius
+                                              child: Image.network(
+                                                'https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
+                                          ),
+                                          title: Text(
+                                            'Louis',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
+                                          subtitle: Text(
+                                            'Patterson',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
+                                          trailing: Icon(Icons.info_outlined),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 2,
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.symmetric(
+                                      vertical: 5,
+                                      horizontal: 23,
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: ElevatedButton(
+                                            style: ElevatedButton.styleFrom(
+                                              backgroundColor: Color(0xff256FFF),
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                  12,
+                                                ), // <-- Radius
+                                              ),
+                                            ),
+                                            onPressed: () {},
+                                            child: const Text(
+                                              'ACCEPT',
+                                              style: TextStyle(
+                                                color: Colors.white70,
+                                                fontWeight: FontWeight.w600,
+                                                fontSize: 12,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 15,
+                                        ),
+                                        ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            backgroundColor: Color(0xffEBEFFA),
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(
+                                                12,
+                                              ), // <-- Radius
+                                            ),
+                                          ),
+                                          onPressed: () {},
+                                          child: const Text(
+                                            'DECLINE',
+                                            style: TextStyle(
+                                              color: Colors.black54,
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ],
