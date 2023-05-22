f4af2b4bc65132f62f52c7bfc1f1197cc5568953
Cesar Morales <cesar.aml1891@gmail.com>
2023-05-21 23:44:34 -0500

    Definiendo Container con SafeArea con column para demas widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text(
-          "Prueba",
+      body: Container(
+        color: const Color(0xffD5F3F4),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 22,
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
         ),
       ),
     );
