c076346624a07b817e5f99e67489a0e95d3b1c03
Cesar Morales <cesar.aml1891@gmail.com>
2023-05-22 00:35:18 -0500

    Acabando iconos superiores

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,17 +5,41 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: double.infinity,
         color: const Color(0xffD5F3F4),
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(
-              horizontal: 15,
+              horizontal: 30,
               vertical: 22,
             ),
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.red,
+                color: Colors.white.withOpacity(0.5),
                 borderRadius: BorderRadius.circular(30),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 30,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(
+                          Icons.dehaze,
+                          color: Colors.black45,
+                        ),
+                        Icon(
+                          Icons.calendar_today,
+                          color: Colors.black45,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
